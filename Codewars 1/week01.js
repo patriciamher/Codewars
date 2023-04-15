be336addb52a65b08f99b37d50541b9657471ad6
patriciamher <hernandezpatriciam@hotmail.com>
2023-04-15 11:04:30 -0400

    first commit

--- Codewars 1/week01.js
+++ Codewars 1/week01.js
@@ -37,16 +37,15 @@ function past(h, m, s){
     const convertSeconds = s * 1000
     return convertHours + convertMins + convertSeconds;
   }
-
 //You get an array of numbers, return the sum of all of the positives ones. Example [1,-4,7,12] => 1 + 7 + 12 = 20
 //Note: if there is nothing to sum, the sum is default to 0.
 
 function positiveSum(arr) {
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++){
-      if(arr[i] > 0){
-        sum = sum + arr[i];
-      }
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > 0){
+      sum = sum + arr[i];
     }
-    return sum
   }
+  return sum
+}
