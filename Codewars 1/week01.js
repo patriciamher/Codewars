2ae2b2a1c44df129ec9097d56420abaf9b0ad52f
patriciamher <hernandezpatriciam@hotmail.com>
2023-04-22 11:06:48 -0400

    first commit

--- Codewars 1/week01.js
+++ Codewars 1/week01.js
@@ -15,18 +15,18 @@ function greet(name){
 //Let's play! You have to return which player won! In case of a draw return Draw!. Examples(Input1, Input2 --> Output):
 
 const rps = (p1, p2) => {
-    if (p1 == p2){
-        return "Draw!";
-    }
-    
-    else if ((p1 == "scissors" && p2 == "paper") || (p1 == "paper" && p2 == "rock") || (p1 == "rock" && p2 == "scissors") ){
-        return "Player 1 won!";
-    }
-    
-    else { 
-        return "Player 2 won!";
-    }
+  if (p1 == p2){
+      return "Draw!";
   }
+  
+  else if ((p1 == "scissors" && p2 == "paper") || (p1 == "paper" && p2 == "rock") || (p1 == "rock" && p2 == "scissors") ){
+      return "Player 1 won!";
+  }
+  
+  else { 
+      return "Player 2 won!";
+  }
+}
 
 //Clock shows h hours, m minutes and s seconds after midnight.
 //Your task is to write a function which returns the time since midnight in milliseconds.
