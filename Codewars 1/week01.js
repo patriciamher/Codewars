0a72513be3dcb866c11caacc56b954b21b86e1fd
patriciamher <hernandezpatriciam@hotmail.com>
2023-04-23 11:07:38 -0400

    first commit

--- Codewars 1/week01.js
+++ Codewars 1/week01.js
@@ -1,8 +1,8 @@
 //Write a function which converts the input string to uppercase.
 
 function makeUpperCase(str) {
-    let upstr = str.toUpperCase()
-    return upstr
+  let upstr = str.toUpperCase()
+  return upstr
 }
 
 //Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
