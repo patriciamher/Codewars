3b23b03ae75ea24b869e9bb34caaa262f2d525fb
patriciamher <hernandezpatriciam@hotmail.com>
2023-05-02 21:32:28 -0400

    first commit

---
+++ break camelcase/app.js
@@ -0,0 +1,15 @@
+//Complete the solution so that the function will break up camel casing, using a space between words.
+//Example
+//"camelCasing"  =>  "camel Casing"
+//"identifier"   =>  "identifier"
+//""             =>  ""
+
+function solution(string) {
+    string = string.split('').map(function (el) {
+      if (el === el.toUpperCase()) {
+        el = ' ' + el
+      }
+      return el
+    })
+    return string.join('')
+  }
